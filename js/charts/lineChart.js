08aa67121c18d7df62c8664c9415aeec56419dd8
akolyte <hryoo2004@gmail.com>
2023-07-16 17:22:33 -0400

    feat: finished pre 1778 chart

--- js/charts/lineChart.js
+++ js/charts/lineChart.js
@@ -2,7 +2,7 @@
 // Chart dimensions
 const width = 400;
 const height = 300;
-const margin = { top: 20, right: 20, bottom: 40, left: 40 };
+const margin = { top: 20, right: 20, bottom: 40, left: 60 };
 const chartWidth = width - margin.left - margin.right;
 const chartHeight = height - margin.top - margin.bottom;
 // Sample data
@@ -18,7 +18,7 @@ async function init() {
 
     // Create the SVG element
     const svg = d3
-    .select('svg')
+    .select('#native-hawaiian-population-pre-1778')
     .append('g')
     .attr('width', width)
     .attr('height', height);
@@ -28,7 +28,6 @@ async function init() {
     .scalePoint()
     .domain(data.map(d => d.year))
     .range([0, chartWidth])
-    .padding(0.5);
 
     // Create a scale for the y-axis
     const yScale = d3
@@ -43,11 +42,38 @@ async function init() {
     .y(d => margin.top + yScale(d.population));
 
     // Append the line path
-    svg
-    .append('path')
-    .datum(data)
-    .attr('d', line)
-    .attr('fill', 'none')
-    .attr('stroke', 'steelblue')
-    .attr('stroke-width', 2);
+    svg.append('path')
+        .datum(data)
+        .attr('d', line)
+        .attr('fill', 'none')
+        .attr('stroke', 'steelblue')
+        .attr('stroke-width', 2);
+
+    // Append X-axis
+    svg.append('g')
+        .attr("transform",`translate(${margin.left}, ${height - margin.bottom})`)
+        .call(d3.axisBottom(xScale).tickValues([500,700,900,1100,1300,1500,1700]).tickFormat(d3.format('~s')))
+
+    // Append Y-axis
+    svg.append('g')
+        .attr("transform",`translate(${margin.left}, ${margin.top})`)
+        .call(d3.axisLeft(yScale).tickValues([0,50000,100000,150000,200000,250000,300000,350000]).tickFormat(d3.format('~s')))
+
+    // Append x-axis label
+    svg.append("text")
+    .attr("y", height)
+    .attr("x", width/2)
+    .style("text-anchor", "middle")
+    .style("fill", "black")
+    .text("Year");
+
+    // Append y-axis label
+    svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0)
+    .attr("x", -(height/2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("fill", "black")
+    .text("Population"); 
 }
