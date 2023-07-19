1ad11da004b9170e54effe2c77152ebc1d6b86d9
akolyte <hryoo2004@gmail.com>
2023-07-19 12:06:42 -0400

    feat: cleaned data

--- js/utils/reshapeChloroplethData.js
+++ js/utils/reshapeChloroplethData.js
@@ -1,7 +1,5 @@
-const d3 = require('d3');
-
 async function reshape() {
-    const filePath = 'js/data/1900_2020_Population_By_County.csv'
+    const filePath = 'main/js/data/1900_2020_Population_By_County.csv'
     let data = await d3.csv(`https://raw.githubusercontent.com/Akolyte/CS_416_Native_Hawaiian_Population_Narrative_Visualization/${filePath}`)
     let reshapedData = [];
 
@@ -17,7 +15,22 @@ async function reshape() {
         });
     });
 
-    console.log(data)
-}
+    console.log(reshapedData)
+    // Convert data to CSV string
+    const csvData = d3.csvFormat(data);
 
-reshape()
+    // Create a blob from the data
+    const blob = new Blob([csvData], { type: 'text/csv' });
+
+    // Create an anchor element and set the href and download attributes
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = '1900_2020_population_by_county_clean.csv';
+
+    // Append the anchor element to the body and click it to start download
+    document.body.appendChild(a);
+    a.click();
+
+    // Clean up the DOM: remove the anchor element
+    document.body.removeChild(a);
+}
